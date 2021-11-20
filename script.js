23f0aa106d5fedb60514005f5f790176c17ae491
Kyle <kmurray363@gmail.com>
2021-11-20 11:23:16 -0500

    Added functionality for single game of rock paper scissors.

---
+++ script.js
@@ -0,0 +1,68 @@
+function computerSelection(){
+    const choice = Math.floor(Math.random() * 3)
+    let output = ""
+    switch (choice){
+        case 0:
+            output = "rock";
+            break;
+        case 1:
+            output = "paper";
+            break;
+        case 2:
+            output = "scissors";
+            break;
+        
+    }
+    return output;
+}
+
+function playerSelection(){
+    return prompt('Please make your selection. Type either rock, paper or scissors to continue').toLowerCase();
+}
+
+function playRound(computerSelection, playerSelection){
+    let score = 0;
+    while (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors"){
+        playerSelection = prompt('Invalid selection. Please type either rock, paper or scissors to continue').toLowerCase();
+    }
+    if (playerSelection === computerSelection){
+        console.log(`You both picked ${playerSelection} it's a tie."`)
+    } 
+    else {
+        switch (playerSelection){
+            case 'rock':
+                if (computerSelection === 'paper') {
+                    console.log(`You selected ${playerSelection} and your opponent selected ${computerSelection}... You lose this round.`);
+                    score = -1;
+                }
+                else if (computerSelection === 'scissors') {
+                    console.log(`You selected ${playerSelection} and your opponent selected ${computerSelection}... You win this round.`);
+                    score = 1;
+                }
+                break;
+            case 'paper':
+                if (computerSelection === 'scissors') {
+                    console.log(`You selected ${playerSelection} and your opponent selected ${computerSelection}... You lose this round.`);
+                    score = -1;
+                    }
+                else if (computerSelection === 'rock') {
+                    console.log(`You selected ${playerSelection} and your opponent selected ${computerSelection}... You win this round.`);
+                    score = 1;
+                }
+                break;
+            case 'scissors':
+                if (computerSelection === 'rock') {
+                    console.log(`You selected ${playerSelection} and your opponent selected ${computerSelection}... You lose this round.`);
+                    score = -1;
+                    }
+                else if (computerSelection === 'paper') {
+                    console.log(`You selected ${playerSelection} and your opponent selected ${computerSelection}... You win this round.`);
+                    score = 1;
+                }
+                break;
+        }
+    }
+    console.log(score);
+}
+
+playRound(computerSelection(),playerSelection());
